591cb81c820a85811c05b44e97dab3e516cb3393
souverain45 <115586761+souverain45@users.noreply.github.com>
2024-02-15 10:05:22 +0100

    Create mbote.py
    entre ton nom et tu reçois une salutation

---
+++ mbote.py
@@ -0,0 +1,5 @@
+# Demander à l'utilisateur d'entrer son nom
+nom_utilisateur = input("Entrez votre nom : ")
+
+# Afficher le message de bienvenue avec le nom
+print("Bienvenue chez", nom_utilisateur)
